a5f1b35d30d7a6ec805cdc50e6766f84eb44f2b8
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-03-31 11:42:02 -0300

    Realizo ejercicio 13

---
+++ js/ejercicio13/ejercicio13.js
@@ -0,0 +1,10 @@
+"Use strict";
+
+/* Ejercicio con String:
+Realiza un script que pida un texto y lo muestre en mayúsculas. */
+
+let texto = prompt("Escribe un texto.");
+
+let textoEnMayus = texto.toLocaleUpperCase();
+
+document.write(textoEnMayus);
