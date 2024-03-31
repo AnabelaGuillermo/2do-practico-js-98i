453fd54dcaf99d17a281542f1253e9d29376acbd
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-03-31 11:34:51 -0300

    Realizo ejercicio 12

---
+++ js/ejercicio12/ejercicio12.js
@@ -0,0 +1,10 @@
+"Use strict";
+
+/* Ejercicio con Math:
+Realiza un script que genere un número aleatorio entre 1 y 99. */
+
+let numAleatorio;
+
+numAleatorio = Math.floor(Math.random() * 99) + 1;
+
+document.write(numAleatorio);
